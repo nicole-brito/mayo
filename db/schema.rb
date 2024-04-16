9ab19e3f25ec4637796e34272f284f07e09ade35
nic-linux <nicolem.brito@gmail.com>
2024-04-16 16:30:29 -0300

    🗝️  Criado autenticação de usuários

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_145322) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_190930) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_16_145322) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
