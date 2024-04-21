e0bff5bdebb9ac423be18556f5e37b905b90965a
nic <nicolem.britto@gmail.com>
2024-04-21 08:47:10 -0300

    � Adicionando das tags

--- db/migrate/20240417183250_remove_status_from_articles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromArticles < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :articles, :status, :string
-  end
-end
--- db/migrate/20240417183258_remove_status_from_comments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromComments < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :comments, :status, :string
-  end
-end
